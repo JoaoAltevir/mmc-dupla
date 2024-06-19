823c3b354d0e7846c8aebd29b5bfa6a0a4ce4817
Neto <j.altevirneto@gmail.com>
2024-06-19 08:26:15 -0300

    feat: add processo de leitura

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-//iniciando o código
+let numA;
+let numB;
+let numAtual;
+
+console.log("Informe o primeiro número para fazer o mmc:");
+process.stdin.on("data", function (data){
+    if(!numA){
+        numA = Number(data.toString().trim());
+        console.log("Agora, informe o segundo:");
+    }else if(!numB){
+        numB = Number(data.toString().trim());
+    } else {
+        while(numA != numB){
+            
+        }
+    }
+})
